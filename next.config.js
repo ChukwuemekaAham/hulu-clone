99692b266220f7d13cd94d50bd76180dc7a0c9af
Andrew.Aham <ahamchukwuemeka2@gmail.com>
2022-03-21 15:04:37 +0100

    Added all files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "links.papareact.com",
+      "coaching.papareact.com",
+      "image.tmdb.org",
+      "api.themoviedb.org",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
